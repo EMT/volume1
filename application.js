7399e6da4a24dc39f3b535ac71e57cf5cb50158c
joeforshaw <joe.d.forshaw@gmail.com>
2013-08-16 12:31:38 +0100

    Initial commit

---
+++ application.js
@@ -0,0 +1,24 @@
+// $(function () {
+// 	var window_width = $(window).width() - $('#page').width();
+
+// 	console.log($('#page').width());
+
+// 	var document_height = $(document).height() - $(window).height();
+
+//     $(window).scroll(function () {
+//         var scroll_position = $(window).scrollTop();
+//         var object_position_left = window_width * (scroll_position / document_height);
+//         $('#page').css({
+//             'left': object_position_left
+//         });
+//     });
+// });
+
+$(function() {
+
+   	$("body").mousewheel(function(event, delta) {
+      	this.scrollLeft -= (delta * 3);
+      	event.preventDefault();
+   	});
+   	
+});
